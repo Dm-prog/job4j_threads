0c8114b343acd6714f5eb83c4e1f2677d79ad61a
Dmitriy <dmitriyk80@bk.ru>
2022-02-24 16:19:30 +0300

    1.1. Состояние нити. [#229175 #263264]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -2,18 +2,24 @@ package ru.job4j.concurrent;
 
 public class Wget {
     public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        for (int index = 0; index < 100; index++) {
-                            System.out.print("\rLoading : " + index + "%\n");
-                            Thread.sleep(1000);
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
+        Thread first = new Thread(
+                () -> {}
         );
-        thread.start();
+        Thread second = new Thread(
+                () -> {}
+        );
+        System.out.println(first.getName() + " " + first.getState());
+        System.out.println(second.getName() + " " +second.getState());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName());
+        }
+        while (second.getState() != Thread.State.TERMINATED) {
+            System.out.println(second.getName());
+        }
+        System.out.println(first.getName() + " " + first.getState());
+        System.out.println(second.getName() + " " + second.getState());
+        System.out.println(Thread.currentThread().getName() + " Работа завершена");
     }
 }
